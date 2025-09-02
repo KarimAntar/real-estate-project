88eb0063cdcc45b2b76e8dad9189d9017ec2c1c9
Karim Mamdouh <101858852+KarimMamdoh@users.noreply.github.com>
2025-09-02 22:19:42 +0300

    General Fix

--- src/app/dashboard/listings/add/page.tsx
+++ src/app/dashboard/listings/add/page.tsx
@@ -1,5 +1,8 @@
+// src/app/dashboard/listings/add/page.tsx
 import DashboardLayout from "@components/dashboard/DashboardLayout";
 import ListingAddClient from "./ListingAddClient";
+
+export const dynamic = "force-dynamic"; // ⬅️ disable static prerendering
 
 export default function Page() {
   return (
